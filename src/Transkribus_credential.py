59fd9804bc4f87b9fc3f65396a6f981bb01ffbf7
Jean-Luc Meunier <jean-luc.meunier@xrce.xerox.com>
2016-11-29 16:58:56 +0100

    some better comment

--- src/Transkribus_credential.py
+++ src/Transkribus_credential.py
@@ -4,6 +4,8 @@
 Store in this file your transkribus credentials.
 Change acess right to protect this information.
 
+Alternatively, use do_login --persist to make a persistent session usable by next commands.
+
 Created on 15 Nov 2016
 
 @author: meunier
