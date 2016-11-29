cae76e6a538762c2c0d2f429c466cada59080f86
Jean-Luc Meunier <jean-luc.meunier@xrce.xerox.com>
2016-11-29 16:30:01 +0100

    ok

--- src/Transkribus_credential.py
+++ src/Transkribus_credential.py
@@ -9,5 +9,7 @@ Created on 15 Nov 2016
 @author: meunier
 """
 
+# Either you store your credentials here, or you use the --persist options
+
 login    = "jean-luc.meunier@xrce.xerox.com"
 password = "my_password_was_here"
